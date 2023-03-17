3e009bd085bba88f878e7472ae1e6254d162d408
Razmoth <12517189-Razmoth@users.noreply.gitlab.com>
2023-03-17 17:33:57 +0400

    Support list loading

--- AssetStudioGUI/AssetStudioGUIForm.cs
+++ AssetStudioGUI/AssetStudioGUIForm.cs
@@ -166,6 +166,10 @@ namespace AssetStudioGUI
                 }
                 else
                 {
+                    if (paths.Length == 1 && File.Exists(paths[0]) && Path.GetExtension(paths[0]) == ".txt")
+                    {
+                        paths = File.ReadAllLines(paths[0]);
+                    }
                     await Task.Run(() => assetsManager.LoadFiles(paths));
                 }
                 BuildAssetStructures();
